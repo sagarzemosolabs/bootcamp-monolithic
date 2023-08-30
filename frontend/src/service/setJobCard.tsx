7115910e29997417f0a1b995bf31cc0ece832553
sagarzemosolabs <sagar.yadav@zemosolabs.com>
2023-08-30 20:35:39 +0530

    Fixes

--- frontend/src/service/setJobCard.tsx
+++ frontend/src/service/setJobCard.tsx
@@ -4,7 +4,7 @@ export const setJobCard = async (url: string) => {
   const response = await API.post(url, {
     headers: {
       'Content-Type': 'application/json;charset=UTF-8',
-      'Access-Control-Allow-Origin': 'http://localhost:8000',
+      'Access-Control-Allow-Origin': 'http://52.90.252.150:8000',
     },
   });
   return response.data;
